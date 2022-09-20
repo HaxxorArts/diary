b8200d4a3c2d3fd45307560052a638df283870f1
a <a@n>
2022-09-20 16:33:56 +0300

    commit 14

---
+++ src/components/StudentInfo.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="info">
+        <table>
+            <tr>
+                <th>Модуль</th>
+                <th>Факторы</th>
+                <th>Обьем занятий(недель)</th>
+                <th>Контрольн. дата</th>
+                <th>Мин. балл</th>
+                <th>Макс. балл</th>
+                <th>Фактич. балл</th>
+                <th>Рейтинг</th>
+                <th>Примечание</th>
+            </tr>
+            <tr>
+                <td>{{info.module}}</td>
+                <td>{{info.factor}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            info: [
+                {
+                    module: 'module',
+                    factor: 'factor',
+
+                }
+            ]
+        }
+    }
+}
+</script>
+<style scoped>
+    .info {
+        margin-top: 20px;
+        padding: 10px;
+    }
+    .info table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+    .info th, td {
+        border: 1px solid black;
+        text-align: left;
+        padding: 15px 10px;
+    }
+</style>
--- src/pages/Signed.vue
+++ src/pages/Signed.vue
@@ -1,14 +1,16 @@
 <template>
     <div>
         <MyLabel>You've been done authorization successfully</MyLabel>
+        <StudentInfo></StudentInfo>
     </div>
 </template>
 
 <script>
 import MyButton from '@/components/UI/MyButton.vue';
 import MyLabel from '@/components/UI/MyLabel.vue';
+import StudentInfo from '@/components/StudentInfo.vue';
 export default {
-    components: { MyButton, MyLabel },
+    components: { MyButton, MyLabel, StudentInfo },
     methods: {
         
     },
