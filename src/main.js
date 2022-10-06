5eaed3e2d7e44323078109f330a09be8b0fc58f8
a <a@n>
2022-10-06 11:48:43 +0300

    BoostrapVue3 added

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import router from "@/router/router";
 import store from '@/store';
 import BootstrapVue3 from 'bootstrap-vue-3';
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+
 const app = createApp(App)
 
 components.forEach(component => {
